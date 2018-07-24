44c1ddf7c23ac45646092c0b20b9406764cc6872
Raquel Villaverde <raquelv85@gmail.com>
2018-07-24 20:52:32 +0200

    create rama-footer and add footer component

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import Header from './Header.js';
-import Footer from './Footer.js';
+import Footer from './components/Footer.js';
 import './Main.css';
 
 class Main extends Component {
   render() {
     return (
       <Fragment>
-      {/* <Header/>
-      <Footer/> */}
+      {/* <Header/>*/}
+      <Footer/> 
       </Fragment>
     );
   }
