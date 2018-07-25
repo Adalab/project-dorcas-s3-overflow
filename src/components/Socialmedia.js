a1933a952744d4d0c869f98be794cd717e2f2f5c
Olatz Aranzabe <olatzaranzabe@gmail.com>
2018-07-25 20:57:42 +0200

    work on social media links

---
+++ src/components/Socialmedia.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Socialmedia extends Component {
+    render() {
+
+        return (
+            <div className="logos-container">
+                <a className="boton-red-social tfn_button phone_button local--phone" id="element-telefono" href="">
+                    <i className="fas fa-mobile-alt logo-preview logo-fono"></i>
+                </a>
+                {/* <a class="boton-red-social email_button local--email" id="element-email" href="">
+                    <i class="far fa-envelope logo-preview logo-carta"></i>
+                </a>
+                <a class="boton-red-social linkedin_button local--linkedin" id="element-linkedin" href="">
+                    <i class="fab fa-linkedin-in logo-preview logo-in"></i>
+                </a>
+                <a class="boton-red-social github_button local--github" id="element-github" href="">
+                    <i class="fab fa-github-alt logo-preview logo-github"></i>
+                </a> */}
+            </div>
+        );
+    }
+}
+
+export default Socialmedia;
