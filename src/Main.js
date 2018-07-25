aefbce26d6e441c128689446c717917aac8da3ee
valromo <valroldanmotos@gmail.com>
2018-07-25 21:06:35 +0200

    wip-formulario

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Preview from './components/Preview.js';
+import Formulario from './components/Formulario.js';
 import './Main.css';
 
 class Main extends Component {
