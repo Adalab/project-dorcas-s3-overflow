2e7c15af401d5f543435fba0fe944fd6cce8c31a
valromo <valroldanmotos@gmail.com>
2018-08-05 11:29:45 +0200

    create page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,217 +1,210 @@
 import React, { Component, Fragment } from 'react';
-import Header from './Header.js';
-import Footer from './Footer.js';
 import Preview from './Preview.js';
 import Formulario from './Formulario.js';
-import './Main.css';
-
 // mapeado de clases
 const paletteClass = {
-  "1": 'greenTarget',
-  "2": 'redTarget',
-  "3": 'greyTarget'
-}
-
-const fontClass = {
-  "1": 'ubuntuFont',
-  "2": 'comicFont',
-  "3": 'montFont'
-}
-
-class Main extends Component {
-  constructor() {
-    super()
-
-    this.state = {
-      data: {
-        name: '',
-        job: '',
-        image: '',
-        email: '',
-        phone: '',
-        github: '',
-        linkedin: '',
-        skills: [],
-        palette: '',
-        typography: ''
-      },
-
-      skillOptions: []
+    "1": 'greenTarget',
+    "2": 'redTarget',
+    "3": 'greyTarget'
+  }
+  
+  const fontClass = {
+    "1": 'ubuntuFont',
+    "2": 'comicFont',
+    "3": 'montFont'
+  }
+  
+  class Main extends Component {
+    constructor() {
+      super()
+  
+      this.state = {
+        data: {
+          name: '',
+          job: '',
+          image: '',
+          email: '',
+          phone: '',
+          github: '',
+          linkedin: '',
+          skills: [],
+          palette: '',
+          typography: ''
+        },
+  
+        skillOptions: []
+      }
+  
+      // rellena inputs binds
+  
+      this.handleNameInput = this
+        .handleNameInput
+        .bind(this);
+      this.handleJobInput = this
+        .handleJobInput
+        .bind(this);
+      this.handleEmailInput = this
+        .handleEmailInput
+        .bind(this);
+      this.handlePhoneInput = this
+        .handlePhoneInput
+        .bind(this);
+      this.handleGithubInput = this
+        .handleGithubInput
+        .bind(this);
+      this.handleLinkedinInput = this
+        .handleLinkedinInput
+        .bind(this);
+      this.fileInput = React.createRef();
+      this.handleAddImage = this
+        .handleAddImage
+        .bind(this);
+      this.handleClickImage = this
+        .handleClickImage
+        .bind(this);
+  
+        this.getSkills();
     }
-
-    // rellena inputs binds
-
-    this.handleNameInput = this
-      .handleNameInput
-      .bind(this);
-    this.handleJobInput = this
-      .handleJobInput
-      .bind(this);
-    this.handleEmailInput = this
-      .handleEmailInput
-      .bind(this);
-    this.handlePhoneInput = this
-      .handlePhoneInput
-      .bind(this);
-    this.handleGithubInput = this
-      .handleGithubInput
-      .bind(this);
-    this.handleLinkedinInput = this
-      .handleLinkedinInput
-      .bind(this);
-    this.fileInput = React.createRef();
-    this.handleAddImage = this
-      .handleAddImage
-      .bind(this);
-    this.handleClickImage = this
-      .handleClickImage
-      .bind(this);
-
-      this.getSkills();
-  }
-
-
-
-  handleClickImage(event) {
-    console.log(event.target.files)
-    console.log(this.setState)
-    const fr = new FileReader();
-    fr.addEventListener('load', () => {
-      this.setState({ image: fr.result });
-    });
-    fr.readAsDataURL(event.target.files[0]);
-
-  }
-
-  handleAbilities() {
-    console.log('habilidad añadida')
-  }
-
-  // handle rellena inputs
-
-  handleNameInput(event) {
-    this.setState({
-      data: {
-        ...this.state.data,
-        name: event.target.value
-      }
-    })
-  }
-
-  handleJobInput(event) {
-    this.setState({
-      data: {
-        ...this.state.data,
-        job: event.target.value
-      }
-    })
-  }
-
-  handleEmailInput(event) {
-    this.setState({
-      data: {
-        ...this.state.data,
-        email: event.target.value
-      }
-    })
-  }
-
-  handlePhoneInput(event) {
-    this.setState({
-      data: {
-        ...this.state.data,
-        phone: event.target.value
-      }
-    })
-  }
-
-  handleGithubInput(event) {
-    this.setState({
-      data: {
-        ...this.state.data,
-        github: event.target.value
-      }
-    })
-  }
-
-  handleLinkedinInput(event) {
-    this.setState({
-      data: {
-        ...this.state.data,
-        linkedin: event.target.value
+  
+  
+  
+    handleClickImage(event) {
+      console.log(event.target.files)
+      console.log(this.setState)
+      const fr = new FileReader();
+      fr.addEventListener('load', () => {
+        this.setState({ image: fr.result });
+      });
+      fr.readAsDataURL(event.target.files[0]);
+  
     }
-})
-}
-
-  // handle imagen load
-
-  handleAddImage(event) {
-    this
-      .fileInput
-      .current
-      .click()
-  }
-
-  handleClickImage(event) {
-    const fr = new FileReader();
-
-    fr.addEventListener('load', () => {
+  
+    handleAbilities() {
+      console.log('habilidad añadida')
+    }
+  
+    // handle rellena inputs
+  
+    handleNameInput(event) {
       this.setState({
         data: {
           ...this.state.data,
-          image: fr.result
+          name: event.target.value
         }
       })
-    })
-
-    fr.readAsDataURL(event.target.files[0]);
-
+    }
+  
+    handleJobInput(event) {
+      this.setState({
+        data: {
+          ...this.state.data,
+          job: event.target.value
+        }
+      })
+    }
+  
+    handleEmailInput(event) {
+      this.setState({
+        data: {
+          ...this.state.data,
+          email: event.target.value
+        }
+      })
+    }
+  
+    handlePhoneInput(event) {
+      this.setState({
+        data: {
+          ...this.state.data,
+          phone: event.target.value
+        }
+      })
+    }
+  
+    handleGithubInput(event) {
+      this.setState({
+        data: {
+          ...this.state.data,
+          github: event.target.value
+        }
+      })
+    }
+  
+    handleLinkedinInput(event) {
+      this.setState({
+        data: {
+          ...this.state.data,
+          linkedin: event.target.value
+      }
+  })
   }
-
-  getSkills() {
-    fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json')
-      .then((response) => response.json())
-      .then((jsonskills) => {
+  
+    // handle imagen load
+  
+    handleAddImage(event) {
+      this
+        .fileInput
+        .current
+        .click()
+    }
+  
+    handleClickImage(event) {
+      const fr = new FileReader();
+  
+      fr.addEventListener('load', () => {
         this.setState({
-          skillOptions: jsonskills.skills
+          data: {
+            ...this.state.data,
+            image: fr.result
+          }
+        })
+      })
+  
+      fr.readAsDataURL(event.target.files[0]);
+  
+    }
+  
+    getSkills() {
+      fetch('https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json')
+        .then((response) => response.json())
+        .then((jsonskills) => {
+          this.setState({
+            skillOptions: jsonskills.skills
+          });
         });
-      });
+    }
+  
+    render() {
+      console.log('skills', this.state.skills)
+      const userInfo = this.state.data
+      return (
+          <main className="container-mediaqueries-preview">
+            <Preview
+              name={userInfo.name}
+              job={userInfo.job}
+              email={userInfo.email}
+              phone={userInfo.phone}
+              github={userInfo.github}
+              linkedin={userInfo.linkedin}
+              photo={userInfo.photo}
+              skills={userInfo.skills}
+              paletteClass={paletteClass[userInfo.palette]}
+              typographyClass={fontClass[userInfo.typography]}
+            />
+            <Formulario 
+              userInfo = {this.state.data}
+              onInputNameChange = {this.handleNameInput}
+              onInputJobChange =  {this.handleJobInput}
+              onInputEmailChange = {this.handleEmailInput}
+              onInputPhoneChange = {this.handlePhoneInput}
+              onInputGitChange = {this.handleGithubInput}
+              onInputLinkedinChange = {this.handleLinkedinInput}
+              skillOptions={this.state.skillOptions}
+            />
+          </main>
+         
+      );
+    }
   }
-
-  render() {
-    console.log('skills', this.state.skills)
-    const userInfo = this.state.data
-    return (
-      <Fragment>
-        <Header />
-        <main className="container-mediaqueries-preview">
-          <Preview
-            name={userInfo.name}
-            job={userInfo.job}
-            email={userInfo.email}
-            phone={userInfo.phone}
-            github={userInfo.github}
-            linkedin={userInfo.linkedin}
-            photo={userInfo.photo}
-            skills={userInfo.skills}
-            paletteClass={paletteClass[userInfo.palette]}
-            typographyClass={fontClass[userInfo.typography]}
-          />
-          <Formulario 
-            userInfo = {this.state.data}
-            onInputNameChange = {this.handleNameInput}
-            onInputJobChange =  {this.handleJobInput}
-            onInputEmailChange = {this.handleEmailInput}
-            onInputPhoneChange = {this.handlePhoneInput}
-            onInputGitChange = {this.handleGithubInput}
-            onInputLinkedinChange = {this.handleLinkedinInput}
-            skillOptions={this.state.skillOptions}
-          />
-        </main>
-        <Footer />
-      </Fragment>
-    );
-  }
-}
-
-export default Main;
+  
+  export default Main;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
-import Main from './components/Main.js';
+import Page from './components/Page.js';
 import fontAwesomeStyles from '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(
     <HashRouter>
-      <Main />
+      <Page />
     </HashRouter>,
     document.getElementById('root')
   );
