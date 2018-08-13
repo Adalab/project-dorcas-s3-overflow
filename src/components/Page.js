097e2432c36f7718b457c0183171585c49772f5a
Esther Pato <estherpato@gmail.com>
2018-08-13 14:44:34 +0200

    test

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import Main from './Main.js';
-import './Page.css';
+import '../scss/Page.css';
 
 
 class Page extends Component {
